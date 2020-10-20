ce2e354ad4868ac3250e2817880b4f9e2597d037
Kevin Koobs <kevinkoobs@impres.nl>
2020-10-20 15:14:18 +0200

    JS30-11: Update toggle button on pause and play

--- JS30-11/scripts.js
+++ JS30-11/scripts.js
@@ -11,5 +11,14 @@ const   player = document.querySelector('.player'),
 function togglePlay() {
 	video.paused ? video.play() : video.pause();
 }
+function updateButton() {
+	toggle.textContent = this.paused ? '►' : '❚ ❚';
+}
 
 // Hook up event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+
