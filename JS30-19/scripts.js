d30294e017bbd660b9d4d87398a67582e0633dad
Kevin Koobs <kevinkoobs@impres.nl>
2020-10-28 11:48:37 +0100

    JS30-19: build video getter and installed npm files

--- JS30-19/scripts.js
+++ JS30-19/scripts.js
@@ -3,3 +3,11 @@ const video = document.querySelector('.player'),
 	ctx = canvas.getContext('2d'),
 	strip = document.querySelector('.strip'),
 	snap = document.querySelector('.snap');
+
+function getVideo() {
+	navigator.mediaDevices.getUserMedia({video: true, audio : false})
+		.then(localMediaStream => {
+			console.log(localMediaStream);
+		});
+}
+getVideo();
