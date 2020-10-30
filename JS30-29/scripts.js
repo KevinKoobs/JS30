cb96248f422056f80673a228f7d9d8c8afdee73e
Kevin Koobs <kevinkoobs@impres.nl>
2020-10-30 14:03:45 +0100

    JS30-29: Update title and timer

--- JS30-29/scripts.js
+++ JS30-29/scripts.js
@@ -1,15 +1,31 @@
 let countDown;
-function timer(seconds) {
+const timerDisplay = document.querySelector('.display__time-left');
+
+
+function timer(seconds)
+{
 	const now = Date.now(),
 		then = now + seconds * 1000; // Then is in milliseconds, so we need to multiply it by 1000
 
+	displayTimeLeft(seconds);
+
 	countDown = setInterval(() => {
-		const secondsLeft = Math.round(then - Date.now() / 1000); // Date.now and then are in milliseconds
+		const secondsLeft = Math.round((then - Date.now()) / 1000); // Date.now and then are in milliseconds
 		// Check if we should stop it
-		if(secondsLeft <= 0) {
+		if (secondsLeft < 0) {
 			clearInterval(countDown);
+			return;
 		}
-	}, 1000)
+		displayTimeLeft(secondsLeft);
+	}, 1000);
 }
 
-timer();
+function displayTimeLeft(seconds)
+{
+	console.log(seconds);
+	const minutes = Math.floor(seconds / 60);
+	const remainderSeconds = seconds % 60;
+	const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+	document.title = display;
+	timerDisplay.textContent = display;
+}
